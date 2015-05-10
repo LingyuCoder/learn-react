71cfce7a8fb128b6a954bbdcc356df90a42b1781
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-05-10 13:38:08 +0800

    add checkbox and tagfield

--- page/calculator/index.jsx
+++ page/calculator/index.jsx
@@ -49,7 +49,7 @@ var ButtonPanel = React.createClass({
                 event.stopPropagation();
                 event.preventDefault();
             }
-        }
+        };
     },
     render: function() {
         return (
