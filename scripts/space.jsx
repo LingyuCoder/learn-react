c4b43d0ee731379b0fc1d66b34a00b5188f5fb99
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-05-18 00:06:46 +0800

    rebuild

--- scripts/space.jsx
+++ scripts/space.jsx
@@ -10,7 +10,6 @@ let randomDuration = () => {
 
 const transitionsIn = [
 	'scaleIn',
-	'fadeIn',
 	'rotateInQuarterClockwise',
 	'slideInBottom',
 	'slideInRight',
@@ -20,7 +19,6 @@ const transitionsIn = [
 
 const transitionsOut = [
 	'scaleOut',
-	'fadeOut',
 	'zoomOut',
 	'rotate3dOut',
 	'slideOutDown',
@@ -49,70 +47,70 @@ var space = React.render(
 	<AsSpace selector=".custom-frame">
 		<span className="fix prev" onClick={prevCallback}>&lt;</span>
 		<span className="fix next" onClick={nextCallback}>&gt;</span>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<h1>我的滑板鞋</h1>	
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/1.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/2.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/3.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/4.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/5.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/6.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/7.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/8.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/9.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/10.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/11.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/12.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/13.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/14.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/15.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/16.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/17.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/18.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/19.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<img src="http://skyinlayerblog.qiniudn.com/huabanxie/20.jpg" alt=""/>
 		</AsSpaceFrame>
-		<AsSpaceFrame classes={['custom-frame']} exit={randomTransitionOut()} enter={randomTransitionIn()}>
+		<AsSpaceFrame classes={['custom-frame']} exit={"fadeOut " + randomTransitionOut()} enter={"fadeIn" + randomTransitionIn()}>
 			<h1>完</h1>
 		</AsSpaceFrame>
 	</AsSpace>,
